ec49e33fa195d70cc5734671b42513c05e2f7e76
조승후 <37996446+whtmdgn1409@users.noreply.github.com>
2024-05-28 21:48:43 +0900

    Create 주차요금계산.js

---
+++ 주차요금계산.js
@@ -0,0 +1,33 @@
+function solution(fees, records) {
+    const cars = {};
+    
+    records.forEach((record) => {
+        let [time, car, type] = record.split(" ")
+        const [hour, minute] = time.split(":")
+        time = hour * 60 + Number(minute)
+        
+        if(!cars[car]){
+            cars[car] = {time: 0, car}
+        }
+        
+        cars[car].type = type
+        
+        if(type === "OUT"){
+            cars[car].time += time - cars[car].lastInTime
+            return
+        }
+        cars[car].lastInTime = time
+    })
+    
+    return Object.values(cars)
+        .sort((a,b) => a.car - b.car)
+        .map(v => {
+        if(v.type === "IN"){
+            v.time += 1439 - v.lastInTime
+        }
+        if(fees[0] > v.time){
+            return fees[1]
+        }
+        return fees[1] + Math.ceil((v.time - fees[0]) / fees[2]) * fees[3]
+    })
+}
